8d39fdb919b5a8cb3fa37db92282bef4ff2d24e5
unknown <bstefanskicom@gmail.com>
2018-09-27 13:39:00 +0200

    Spring initializr

---
+++ src/main/java/pl/bmstefanski/website/WebsiteApplication.java
@@ -0,0 +1,13 @@
+package pl.bmstefanski.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteApplication.class, args);
+	}
+	
+}
