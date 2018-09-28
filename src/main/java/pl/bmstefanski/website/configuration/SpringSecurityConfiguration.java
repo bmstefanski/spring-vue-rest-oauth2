dde4331ae7785e967b1d4ae1a299d6decb83e0f4
unknown <bstefanskicom@gmail.com>
2018-09-29 00:01:31 +0200

    Remove resources handling boilerplate code

--- src/main/java/pl/bmstefanski/website/configuration/SpringSecurityConfiguration.java
+++ src/main/java/pl/bmstefanski/website/configuration/SpringSecurityConfiguration.java
@@ -3,7 +3,6 @@ package pl.bmstefanski.website.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
@@ -31,12 +30,6 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
         .and()
         .authorizeRequests()
         .antMatchers("/", "/home").permitAll();
-  }
-
-  @Override
-  public void configure(WebSecurity web) throws Exception {
-    web.ignoring()
-        .antMatchers("/resources/**");
   }
 
 }
